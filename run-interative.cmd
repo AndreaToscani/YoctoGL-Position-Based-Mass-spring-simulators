df8f1132e5bdde57a95abf515d3ec1ae2f5d29e2
AndreaToscani <gialloworld@gmail.com>
2024-02-23 22:56:34 +0100

    Initial commit

---
+++ run-interative.cmd
@@ -0,0 +1,18 @@
+.\bin\yparticle --scene tests\01_particles\particles.json --solver mass_spring --interactive
+.\bin\yparticle --scene tests\02_particles\particles.json --solver mass_spring --interactive
+.\bin\yparticle --scene tests\03_cloth\cloth.json --solver mass_spring --interactive
+.\bin\yparticle --scene tests\04_cloth\cloth.json --solver mass_spring --interactive
+
+.\bin\yparticle --scene tests\01_particles\particles.json --solver position_based --interactive
+.\bin\yparticle --scene tests\02_particles\particles.json --solver position_based --interactive
+.\bin\yparticle --scene tests\03_cloth\cloth.json --solver position_based --interactive
+.\bin\yparticle --scene tests\04_cloth\cloth.json --solver position_based --interactive
+
+
+
+
+
+
+
+
+.\bin\yparticle --scene tests\05_tests\particles.json --solver swarm --interactive --planet earth --frames 300
